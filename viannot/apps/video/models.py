851cd0ad3df374993d31defbebb6d63402c3aff9
andrei-savin <glork2006@gmail.com>
2013-11-16 13:44:53 -0500

    views - authentication

--- viannot/apps/video/models.py
+++ viannot/apps/video/models.py
@@ -1,32 +1,32 @@
 from django.db import models
-
- 
-class User(models.Model) :
-	name = models.CharField(max_length=30)
-	email = models.CharField(max_length=50)
-	pswd = 
+from django.contrib.auth.models import User
 
 class Video(models.Model) :
 	title = models.CharField(max_length=140)
 	url = models.CharField(max_length=200)
 	publisher = models.ForeignKey(User)
-	date = models.DateTimeField(auto_now_add=True)
+	time = models.DateTimeField(auto_now_add=True)
 	up = models.IntegerField('upvotes')
 	down = models.IntegerField('downvotes')
 	original_publisher = models.CharField(max_length=50)
 	original_date = models.DateTimeField()
 	description = models.CharField(max_length=500)
-	
 
 class Comment(models.Model) :
 	author = models.ForeignKey(User)
-	date = models.DateTimeField(auto_now_add=True)
+	time = models.DateTimeField(auto_now_add=True)
 	content = models.CharField(max_length=140)
+	videotime = models.IntegerField('seconds')
+	x = models.IntegerField('x coordinate')
+	y = models.IntegerField('y coordinates')
 	up = models.IntegerField('upvotes')
 	down = models.IntegerField('downvotes')
-	x_coord = models.IntegerField()
-	y_coord = models.IntegerField()
+
+class Reply(models.Model)
+	comment = models.ForeignKey(Comment)
+	author = models.ForeignKey(User)
+	time = models.DateTimeField(auto_now_add=True)
 
 class Tag(models.Model) :
-        tag_name = models.CharField(max_length=50)
-        video_tagged = models.ForeignKey(Video)
+    tag_name = models.CharField(max_length=50)
+    video_tagged = models.ForeignKey(Video)
--- viannot/apps/video/views.py
+++ viannot/apps/video/views.py
@@ -1,6 +1,29 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
 
-# Create your views here.
 
 def home(request) :
-	return render(request, 'home.html', {})
+	if request.user.is_authenticated():
+		logged_in = true
+	else:
+		logged_in = false
+
+	return render(request, 'home.html', {'logged_in' : logged_in})
+
+def login(request) :
+	username = request.POST['username']
+	password = request.POST['password']
+	user = authenticate(username=username, password=password)
+
+	if user is not None :
+		if user.is_active:
+			login(request, user)
+			# redirect to a success page
+		else:
+			# return "disabled account"
+	else:
+		# return "invalid login"
+
+def logout_view(request):
+    logout(request)
