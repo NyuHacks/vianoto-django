6a96012078fb36936762820afcca064dc2890232
Jeehyun Kim <jeehyun.kim1@gmail.com>
2013-11-16 12:19:27 -0500

    models.py

--- viannot/apps/video/models.py
+++ viannot/apps/video/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class User(models.Model):
+    email = models.CharField(max_length=50)
+    password = models.CharField(max_length=8)
+    reg_date_hour = models.
+
+class Video
+    video_url = 
+
+class Comment
+    user_id =
+    x_coord =
+    y_coord =
+
+class 
+    
+class Tag
