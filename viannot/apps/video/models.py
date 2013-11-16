2e6454ce750148d38e6f135352706a9197fa14a0
System Administrator <root@Glorks-MacBook-Air.local>
2013-11-16 12:32:41 -0500

    models

--- viannot/apps/video/models.py
+++ viannot/apps/video/models.py
@@ -1,18 +1,22 @@
 from django.db import models
 
-class User(models.Model):
-    email = models.CharField(max_length=50)
-    password = models.CharField(max_length=8)
-    reg_date_hour = models.
+ 
+class User(models.Model) :
+	name = models.CharField(max_length=30)
+	email = models.CharField(max_length=50)
+	pswd = 
 
-class Video
-    video_url = 
+class Video(models.Model) :
+	title = models.CharField(max_length=140)
+	url = models.CharField(max_length=200)
+	publisher = models.ForeignKey(User)
+	date = models.DateTimeField(auto_now_add=True)
+	up = models.IntegerField('upvotes')
+	down = models.IntegerField('downvotes')
 
-class Comment
-    user_id =
-    x_coord =
-    y_coord =
-
-class 
-    
-class Tag
+class Comment(models.Model) :
+	author = models.ForeignKey(User)
+	date = models.DateTimeField(auto_now_add=True)
+	content = models.CharField(max_length=140)
+	up = models.IntegerField('upvotes')
+	down = models.IntegerField('downvotes')
