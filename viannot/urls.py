e066d8de24cb05c4f7945877e817a06a4e7bc224
System Administrator <root@172-26-167-106.dynapool.nyu.edu>
2013-11-16 05:24:09 -0500

    home view

--- viannot/urls.py
+++ viannot/urls.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from django.conf.urls import patterns, include, url
 
-
 # See: https://docs.djangoproject.com/en/dev/ref/contrib/admin/#hooking-adminsite-instances-into-your-urlconf
 admin.autodiscover()
 
@@ -9,6 +8,7 @@ admin.autodiscover()
 # See: https://docs.djangoproject.com/en/dev/topics/http/urls/
 urlpatterns = patterns('',
     # Admin panel and documentation:
+    url(r'^$', 'apps.video.views.home'),
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
