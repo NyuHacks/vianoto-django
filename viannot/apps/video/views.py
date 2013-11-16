912cb4e6ef3361e610994cb977a51c901f04a6d7
Jeehyun Kim <jeehyun.kim1@gmail.com>
2013-11-16 16:19:16 -0500

    views.py

--- viannot/apps/video/views.py
+++ viannot/apps/video/views.py
@@ -1,29 +1,42 @@
-from django.shortcuts import render
+from django.core.context_processors import csrf
+from django.shortcuts import render_to_response, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login, logout
 
+def loginview(request):
+    c = {}
+    c.update(csrf(request))
+    return render_to_response('login.html', c)
 
-def home(request) :
-	if request.user.is_authenticated():
-		logged_in = True
-	else:
-		logged_in = False
+def auth_and_login(request, onsuccess='/', onfail='/login/'):
+    user = authenticate(username=request.POST['email'], password=request.POST['password'])
+    if user is not None:
+        login(request, user)
+        return redirect(onsuccess)
+    else:
+        return redirect(onfail)  
 
-	return render(request, 'home.html', {'logged_in' : logged_in})
+def create_user(username, email, password):
+    user = User(username=username, email=email)
+    user.set_password(password)
+    user.save()
+    return user
 
-def login(request) :
-	username = request.POST['username']
-	password = request.POST['password']
-	user = authenticate(username=username, password=password)
+def user_exists(username):
+    user_count = User.objects.filter(username=username).count()
+    if user_count == 0:
+        return False
+    return True
 
-	if user is not None :
-		if user.is_active:
-			login(request, user)
-			return render(request, 'home.html', {'logged_in' : True})
-		else:
-			return render(request, 'banned.html', {})
-	else:
-		return render(request, 'login_fail.html', {})
+def sign_up_in(request):
+    post = request.POST
+    if not user_exists(post['email']): 
+        user = create_user(username=post['email'], email=post['email'], password=post['password'])
+            return auth_and_login(request)
+    else:
+            return redirect("/login/")
 
-def logout_view(request):
-    logout(request)
+@login_required(login_url='/login/')
+def secured(request):
+    return render_to_response("secure.html")
